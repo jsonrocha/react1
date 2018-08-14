e8aa33988776e371c2925d95ef7b1a1266c97c7f
Rocha <jrocha@proforma.com>
2018-08-14 14:01:30 -0400

    ExplorerMode

--- react1/src/Movie.js
+++ react1/src/Movie.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+// const key = {}
 
 class Movie extends Component {
   render() {
     return (
       <li className="movie">
-      <h2>{this.props.title}</h2>
-      <img src="https://image.tmdb.org/t/p/w185_and_h278_bestv2/4p1N2Qrt8j0H8xMHMHvtRxv9weZ.jpg" alt='Indy'/>
-      <p>When Dr. Henry Jones Sr. suddenly goes missing while pursuing the Holy Grail, eminent archaeologist Indiana must team up with Marcus Brody, Sallah and Elsa Schneider to follow in his fathers footsteps and stop the Nazis from recovering the power of eternal life.</p>
+      <img src={'https://image.tmdb.org/t/p/w185_and_h278_bestv2' + this.props.poster_path} alt='Indy'/>
+        <h2>{this.props.name}</h2>
+      <p>{this.props.overview}</p>
       </li>
     )
   }
--- react1/src/MovieList.js
+++ react1/src/MovieList.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import Movie from './Movie'
-import results from './movies.json'
+import movies from './movies.json'
 
 class MovieList extends Component {
   render() {
-    console.log(results)
+    console.log(movies)
     return (
       <div className="MovieList">
       <ul>
-      {movies.map((movie, idx) => {
-        <Movie name={movie.title}></Movie>
+      {movies.results.map((movie, idx) => {
+      return  <Movie name={movie.title} poster_path={movie.poster_path} overview={movie.overview}></Movie>
       })}
       </ul>
       </div>
     )
   }
+}
 
 
 export default MovieList
